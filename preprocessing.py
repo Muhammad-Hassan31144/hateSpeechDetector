d0063a8b5f853f7f2bf0814193ee6626a1685b98
Muhammad-Hassan31144 <31144@students.riphah.edu.pk>
2024-06-01 15:25:35 +0500

    Added Preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,13 @@
+import re
+import string
+
+def preprocess_text(text):
+    # Lowercase the text
+    text = text.lower()
+    # Remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # Remove numbers
+    text = re.sub(r'\d+', '', text)
+    # Remove extra whitespace
+    text = ' '.join(text.split())
+    return text
